55e7501032907f0da43e5eb2f6c06a1f6dc8d65c
DESKTOP-KMNS09Q\Admin <quangtuankc040999@gmail.com>
2021-09-01 23:50:50 +0700

    + variable and variable scope in PHP

--- PHP_01/helloWorld.php
+++ PHP_01/helloWorld.php
@@ -1,3 +1,30 @@
 <?php
-echo "Hello World";
+echo "Hello World <br>";
+// comment 
+/**
+ * COMMENT
+ * 
+ */
+$x = 0 ;
+function plusX()
+{
+    global $x;
+    $x++;
+    echo "giá trị của biến x = ".$x."<br>";
+    $x1 = 0;
+    $x1++;
+    echo "giá trị của biến x1 = ".$x1."<br>";
+    static $y = 1;
+    $y ++;
+    echo "giá trị của biến y = ".$y."<br>";
+
+}
+plusX();
+plusX();
+plusX();
+plusX();
+plusX();
+plusX();
+plusX();
+plusX();
 ?>
